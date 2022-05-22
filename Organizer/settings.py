c14612b3652bdacded909ec101b10d777d1c81c0
PrzemekJackowski <przemek.jackowski1304@gmail.com>
2022-05-22 19:55:20 +0200

    Connecting with database

--- Organizer/settings.py
+++ Organizer/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'project_of_organiser',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'Organizer.wsgi.application'
 
 DATABASES = {
     'default': {
+        'NAME': organizer,
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'postgres',
+        'PASSWORD': 'coderslab',
+        'HOST': 'localhost',
     }
 }
 
